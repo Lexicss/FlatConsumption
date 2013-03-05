a9d68f183e0e64b15bc0bcc86bc5923cda339ed4
Aliaksei_Mayorov <Aliaksei_Mayorov@epam.com>
2013-03-05 13:12:40 +0300

    Added Cell object (temporary)

---
+++ FlatConsumption/FCCell.h
@@ -0,0 +1,13 @@
+//
+//  FCCell.h
+//  FlatConsumption
+//
+//  Created by Aliaksei_Maiorau on 3/5/13.
+//  Copyright (c) 2013 Lexicss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCCell : UITableViewCell
+
+@end
---
+++ FlatConsumption/FCCell.m
@@ -0,0 +1,29 @@
+//
+//  FCCell.m
+//  FlatConsumption
+//
+//  Created by Aliaksei_Maiorau on 3/5/13.
+//  Copyright (c) 2013 Lexicss. All rights reserved.
+//
+
+#import "FCCell.h"
+
+@implementation FCCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
